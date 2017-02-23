939aea749a5e5ba7f582ea5fa8bb3db66ec357e0
rjrutkow <rjrutkow@us.ibm.com>
2017-02-23 15:11:48 -0600

    corrected version - ray's Code

--- AdsalesChaincode.go
+++ AdsalesChaincode.go
@@ -47,8 +47,8 @@ type adspot struct {
 
 //This is a broadcaster's inventory
 type releaseInventory struct {
-	LotID              string `json:"lotID"`
-	AdspotId           string `json:"adspotID"`
+	LotId              string `json:"lotId"`
+	AdspotId           string `json:"adspotId"`
 	InventoryDate      string `json:"inventoryDate"`
 	ProgramName        string `json:"programName"`
 	SeasonEpisode      string `json:"seasonEpisode"`
@@ -64,10 +64,38 @@ type releaseInventory struct {
 	//UniqueAdspotID
 }
 
+type queryPlaceOrders struct {
+	LotId              int     `json:"lotId"`
+	AdspotId           int     `json:"adspotId"`
+	ProgramName        string  `json:"programName"`
+	BroadcasterId      string  `json:"broadcasterId"`
+	Genre              string  `json:"genre"`
+	DayPart            string  `json:"dayPart"`
+	TargetGrp          float64 `json:"targetGrp"`
+	TargetDemographics string  `json:"targetDemographics"`
+	InitialCpm         float64 `json:"initialCpm"`
+	Bsrp               float64 `json:"bsrp"`
+	NumberOfSpots      int     `json:"numberofSpots"`
+}
+
+//To place an order for the adspots
+type placeOrders struct {
+	LotId         string `json:"lotId"`
+	AdspotId      string `json:"adspotId"`
+	OrderNumber   string `json:"orderNumber"`
+	ProgramName   string `json:"programName"`
+	AdvertiserId  string `json:"advertiserId"`
+	AdContractId  string `json:"adContractId"`
+	NumberOfSpots string `json:"numberofSpots"`
+}
+
 //This is a pointer to allAdspots
 type AllAdspots struct {
 	UniqueAdspotId []string `json:"uniqueAdspotId"`
 }
+
+const noData string = "NA"
+const noValue int = -1
 
 //For Debugging
 func showArgs(args []string) {
@@ -84,11 +112,17 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	fmt.Println("Launching Init Function")
 
 	broadcasterId := "BroadcasterA"
+	agencyId := "AgencyA"
+	advertiser1Id := "AdvertiserA"
+	advertiser2Id := "AdvertiserB"
 
 	//Create array for all adspots in ledger
 	var AllAdspotsArray AllAdspots
 
 	t.putAllAdspotPointers(stub, AllAdspotsArray, broadcasterId)
+	t.putAllAdspotPointers(stub, AllAdspotsArray, agencyId)
+	t.putAllAdspotPointers(stub, AllAdspotsArray, advertiser1Id)
+	t.putAllAdspotPointers(stub, AllAdspotsArray, advertiser2Id)
 
 	fmt.Println("Init Function Complete")
 	return nil, nil
@@ -100,8 +134,7 @@ func (t *SimpleChaincode) releaseInventory(stub shim.ChaincodeStubInterface, arg
 	fmt.Println("Running releaseInventory")
 
 	var broadcasterID = args[0]
-	var noData = "NA"
-	var increment = 1000
+	var increment = 1
 
 	fmt.Println(broadcasterID)
 
@@ -127,8 +160,8 @@ func (t *SimpleChaincode) releaseInventory(stub shim.ChaincodeStubInterface, arg
 		for x := 0; x < NumberOfSpots; x++ {
 			var ThisAdspot adspot
 
-			ThisAdspot.UniqueAdspotId = string(increment) // ("1000_" + string(increment))
-			ThisAdspot.LotId, _ = strconv.Atoi(releaseInventoryObj.LotID)
+			ThisAdspot.UniqueAdspotId = ("1000_" + strconv.Itoa(increment))
+			ThisAdspot.LotId, _ = strconv.Atoi(releaseInventoryObj.LotId)
 			ThisAdspot.AdspotId, _ = strconv.Atoi(releaseInventoryObj.AdspotId)
 			ThisAdspot.InventoryDate = releaseInventoryObj.InventoryDate
 			ThisAdspot.ProgramName = releaseInventoryObj.ProgramName
@@ -143,22 +176,23 @@ func (t *SimpleChaincode) releaseInventory(stub shim.ChaincodeStubInterface, arg
 			ThisAdspot.Bsrp, _ = strconv.ParseFloat(releaseInventoryObj.Bsrp, 64)
 			ThisAdspot.OrderDate = noData
 			ThisAdspot.AdAgencyId = noData
-			ThisAdspot.OrderNumber = -1
+			ThisAdspot.OrderNumber = noValue
 			ThisAdspot.AdvertiserId = noData
-			ThisAdspot.AdContractId = -1
+			ThisAdspot.AdContractId = noValue
 			ThisAdspot.AdAssignedDate = noData
 			ThisAdspot.CampaignName = noData
 			ThisAdspot.CampaignId = noData
 			ThisAdspot.WasAired = noData
 			ThisAdspot.AiredDate = noData
 			ThisAdspot.AiredTime = noData
-			ThisAdspot.ActualGrp = -1
+			ThisAdspot.ActualGrp = float64(noValue)
 			ThisAdspot.ActualProgramName = noData
 			ThisAdspot.ActualDemographics = noData
 			ThisAdspot.MakupAdspotId = noData
 
 			increment++
-			fmt.Printf("ThisAdspot: %+v", ThisAdspot)
+			fmt.Printf("ThisAdspot: %+v ", ThisAdspot)
+			fmt.Printf("\n")
 			allAdspotPointers.UniqueAdspotId = append(allAdspotPointers.UniqueAdspotId, ThisAdspot.UniqueAdspotId)
 
 			t.putAdspot(stub, ThisAdspot)
@@ -170,16 +204,100 @@ func (t *SimpleChaincode) releaseInventory(stub shim.ChaincodeStubInterface, arg
 	return nil, nil
 }
 
+//STEP 2 Function - Place Orders for ad spots
+func (t *SimpleChaincode) placeOrders(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	fmt.Println("Running placeOrders")
+	showArgs(args)
+
+	agencyId := args[0]
+	broadcasterId := args[1]
+
+	broadcasterAllAdspotsPointers, _ := t.getAllAdspotPointers(stub, broadcasterId)
+	agencyAllAdsportsPointers, _ := t.getAllAdspotPointers(stub, agencyId)
+
+	// loop through all entries
+	for i := 2; i < len(args); i++ {
+
+		// loop through the ad contracts
+		in := args[i]
+		bytes := []byte(in)
+		var placeOrdersObj placeOrders
+		err := json.Unmarshal(bytes, &placeOrdersObj)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Place Orders Object: %+v", placeOrdersObj)
+
+		// now look through the inventory of ad spots, find match
+		for j := 0; j < len(broadcasterAllAdspotsPointers.UniqueAdspotId); j++ {
+			uniqueAdspotKey := broadcasterAllAdspotsPointers.UniqueAdspotId[j]
+			AdSpotObj, _ := t.getAdspot(stub, uniqueAdspotKey)
+
+			spotid, _ := strconv.Atoi(placeOrdersObj.AdspotId)
+
+			advertiserAllAdsportsPointers, _ := t.getAllAdspotPointers(stub, placeOrdersObj.AdvertiserId)
+
+			if AdSpotObj.AdspotId == spotid && AdSpotObj.AdContractId == noValue { // found adspot and  ad spot not taken
+				numberOfSpotsToPurchase, _ := strconv.Atoi(placeOrdersObj.NumberOfSpots)
+
+				fmt.Printf("Inside if AdSpotObj.AdspotId == spotid && AdSpotObj.AdContractId == noValue \n")
+				fmt.Printf("AdsPotObj.AdspotId: %v \n", AdSpotObj.AdspotId)
+				fmt.Printf("spotid: %v \n", spotid)
+				fmt.Printf("AdSpotObj.AdContractId: %v \n", AdSpotObj.AdContractId)
+				fmt.Println("END IF")
+
+				for k := 0; k < numberOfSpotsToPurchase; k++ {
+					if k > 0 { // get the correct ad spot if needed
+						uniqueAdspotKey = broadcasterAllAdspotsPointers.UniqueAdspotId[j+k]
+						AdSpotObj, _ = t.getAdspot(stub, uniqueAdspotKey)
+					}
+					AdSpotObj.AdAgencyId = agencyId
+					AdSpotObj.AdvertiserId = placeOrdersObj.AdvertiserId
+					AdSpotObj.AdContractId, _ = strconv.Atoi(placeOrdersObj.AdContractId)
+					AdSpotObj.OrderNumber, _ = strconv.Atoi(placeOrdersObj.OrderNumber)
+
+					t.putAdspot(stub, AdSpotObj)
+
+					// save all pointers for appropriate ad agency
+					agencyAllAdsportsPointers.UniqueAdspotId = append(agencyAllAdsportsPointers.UniqueAdspotId, AdSpotObj.UniqueAdspotId)
+
+					// save all pointers for appropriate advertiser
+					advertiserAllAdsportsPointers.UniqueAdspotId = append(advertiserAllAdsportsPointers.UniqueAdspotId, AdSpotObj.UniqueAdspotId)
+				}
+				t.putAllAdspotPointers(stub, advertiserAllAdsportsPointers, placeOrdersObj.AdvertiserId)
+				break // break out of for j loop
+			} else {
+				fmt.Printf("NOHIT -  if AdSpotObj.AdspotId == spotid && AdSpotObj.AdContractId == noValue \n")
+				fmt.Printf("AdsPotObj.AdspotId: %v \n", AdSpotObj.AdspotId)
+				fmt.Printf("spotid: %v \n", spotid)
+				fmt.Printf("AdSpotObj.AdContractId: %v \n", AdSpotObj.AdContractId)
+				fmt.Println("END NO HIT on IF")
+			}
+		}
+	}
+
+	t.putAllAdspotPointers(stub, agencyAllAdsportsPointers, agencyId)
+
+	return nil, nil
+}
+
 func (t *SimpleChaincode) putAdspot(stub shim.ChaincodeStubInterface, adspotObj adspot) ([]byte, error) {
 	//marshalling
 	fmt.Println("Launching putAdspot helper function")
+	fmt.Printf("putAdspot obj: %+v ", adspotObj)
+	fmt.Printf("\n")
+
 	bytes, _ := json.Marshal(adspotObj)
 	err := stub.PutState(adspotObj.UniqueAdspotId, bytes)
 	if err != nil {
 		fmt.Println("Error - could not Marshall in putAdspot")
 		//return nil, err
+	} else {
+		fmt.Println("Success - putAdspot putState works")
 	}
-	fmt.Println("Function Complete")
+	fmt.Println("putAdspot Function Complete")
 	return nil, nil
 }
 
@@ -188,49 +306,67 @@ func (t *SimpleChaincode) getAdspot(stub shim.ChaincodeStubInterface, uniqueAdsp
 	fmt.Println("Launching getAdspot helper function")
 	bytes, err := stub.GetState(uniqueAdspotId)
 	if err != nil {
-		fmt.Println("Error - Could not get Unique Adspot ID")
+		fmt.Println("Error - Could not get Unique Adspot ID: %s", uniqueAdspotId)
 		//return nil, err
+	} else {
+		fmt.Println("Success - getAdspot getState worked with Unique Adspot ID %s", uniqueAdspotId)
 	}
 
 	var adspotObj adspot
 	err = json.Unmarshal(bytes, &adspotObj)
 	if err != nil {
-		fmt.Println("Error - could not Unmarshall in getAdspot")
+		fmt.Println("Error - could not Unmarshall in getAdspot - uniqueAdspotID %s", uniqueAdspotId)
+	} else {
+		fmt.Println("Success - Unmarshall in getAdspot good - uniqueAdspotID %s", uniqueAdspotId)
 	}
 
-	fmt.Println("Function Complete")
+	fmt.Printf("getAdspot: %+v ", adspotObj)
+	fmt.Printf("\n")
+	fmt.Println("getAdspot Function Complete")
 	return adspotObj, err
 }
 
-func (t *SimpleChaincode) getAllAdspotPointers(stub shim.ChaincodeStubInterface, broadcasterId string) (AllAdspots, error) {
+func (t *SimpleChaincode) getAllAdspotPointers(stub shim.ChaincodeStubInterface, userId string) (AllAdspots, error) {
 	//unmarshalling
-	fmt.Println("Launching getAllAdspotPointers helper function")
-	bytes, err := stub.GetState(broadcasterId)
+	fmt.Println("Launching getAllAdspotPointers helper function  -  userid: ", userId)
+	bytes, err := stub.GetState(userId)
 	if err != nil {
 		fmt.Println("Error - Could not get Broadcaster ID")
 		//return nil, err
+	} else {
+		fmt.Println("Success - got Broadcaster ID")
 	}
 
 	var allAdspotPointers AllAdspots
 	err = json.Unmarshal(bytes, &allAdspotPointers)
 	if err != nil {
 		fmt.Println("Error - could not Unmarshall within getAllAdspotPointers")
+	} else {
+		fmt.Println("Success - Unmarshall within getAllAdspotPointers")
 	}
 
-	fmt.Println("Function Complete")
+	fmt.Printf("allAdspotsObj: %+v ", allAdspotPointers)
+	fmt.Printf("\n")
+
+	fmt.Println("getAllAdspotPointers Function Complete - userid: ", userId)
+
 	return allAdspotPointers, err
 }
 
-func (t *SimpleChaincode) putAllAdspotPointers(stub shim.ChaincodeStubInterface, allAdspotsObj AllAdspots, broadcasterId string) ([]byte, error) {
+func (t *SimpleChaincode) putAllAdspotPointers(stub shim.ChaincodeStubInterface, allAdspotsObj AllAdspots, userId string) ([]byte, error) {
 	//marshalling
-	fmt.Println("Launching putAllAdspotPointers helper function")
+	fmt.Println("Launching putAllAdspotPointers helper function userid: ", userId)
+	fmt.Printf("putAllAdspotPointers: %+v ", allAdspotsObj)
+	fmt.Printf("\n")
 	bytes, _ := json.Marshal(allAdspotsObj)
-	err := stub.PutState(broadcasterId, bytes)
+	err := stub.PutState(userId, bytes)
 	if err != nil {
 		fmt.Println("Error - could not Marshall in putAllAdspotPointers")
 		//return nil, err
+	} else {
+		fmt.Println("Success - Marshall in putAllAdspotPointers")
 	}
-	fmt.Println("Function Complete")
+	fmt.Println("putAllAdspotPointers Function Complete - userid: ", userId)
 	return nil, nil
 }
 
@@ -267,6 +403,9 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	if function == "releaseInventory" {
 		fmt.Printf("Function is releaseInventory")
 		return t.releaseInventory(stub, args)
+	} else if function == "placeOrders" {
+		fmt.Printf("Function is placeOrders")
+		return t.placeOrders(stub, args)
 	}
 
 	return nil, errors.New("Received unknown function invocation")
